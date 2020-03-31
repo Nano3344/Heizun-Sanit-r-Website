3b42cbe670905103dd2c459c00ae81aa688380d5
Malik Ebers <malik.ebers@web.de>
2020-03-31 14:49:12 +0200

    Fixed mobile footer responsiveness

--- js/index.js
+++ js/index.js
@@ -16,22 +16,20 @@ function closedata() {
   daten.style.display = 'none';
 }
 
-//Navigation
+// Navigation
 
-var navburger = document.querySelector('.burger-menu');
-var fullnav = document.querySelector('.full-nav');
-const closebutton2 = document.querySelector('.closeNav');
+const navburger = document.querySelector('.burger-menu');
+const fullnav = document.querySelector('.full-nav');
+const closebutton2 = document.getElementById('closeNav');
 
 navburger.addEventListener('click', openNav);
 closebutton2.addEventListener('click', closeNav);
 
 function openNav() {
   fullnav.style.display = 'block';
-  blackscreen.style.display = 'block';
 }
 function closeNav() {
   fullnav.style.display = 'none';
-  blackscreen.style.display = 'none';
 }
 
 //Standort
@@ -43,4 +41,7 @@ standortbutton.addEventListener('click', openStandort);
 
 function openStandort() {
   standortpicture.style.display = 'block';
+}
+function closeStandort() {
+  standortpicture.style.display = 'none';
 }
